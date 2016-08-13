e9bcc53d09b87ae78eb8c2a884e82e558da8cec3
chendehao <958689927@qq.com>
2016-08-13 23:49:38 +0800

    添加一个自定义的  CDH_NavigationController 来做为导航控制器, 方便设置导航栏的背景图片

---
+++ MeiTuan/Classes/Controllers/CDH_NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  CDH_NavigationController.swift
+//  MeiTuan
+//
+//  Created by chendehao on 16/8/13.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_NavigationController: UINavigationController {
+
+}
