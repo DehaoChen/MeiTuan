0ef6b0c2758b52b282e9a0a5ccf5ebcbd880b087
chendehao <958689927@qq.com>
2016-08-14 01:52:50 +0800

    完成监听按钮点击事件

--- MeiTuan/Classes/Controllers/CDH_HomeViewController.swift
+++ MeiTuan/Classes/Controllers/CDH_HomeViewController.swift
@@ -13,10 +13,37 @@ class CDH_HomeViewController: UIViewController {
     
     // MARK: - 懒加载自定义的items控件
     lazy var categoryItem : UIBarButtonItem = {
+        // 初始化分类 Item
+        let categoryView = CDH_BarButtonItemView.barButtonItemView(UIImage(named: "icon_category_-1")!, highlightedImage: UIImage(named: "icon_category_highlighted_-1")!, title: "美团", subTitleLabel: "全部分类")
+        let categoryItem = UIBarButtonItem(customView: categoryView)
         
-        
-        let categoryItem = UIBarButtonItem(customView: <#T##UIView#>)
+        // 监听点击事件
+        categoryView.iconButton.addTarget(self, action: #selector(categoryClick), forControlEvents: .TouchUpInside)
+        return categoryItem
     }()
+    lazy var districtItem : UIBarButtonItem = {
+        // 初始化分类 Item
+        let districtView = CDH_BarButtonItemView.barButtonItemView(UIImage(named: "icon_district" )!, highlightedImage: UIImage(named: "icon_district_highlighted")!, title: "广州", subTitleLabel: "全部区域")
+        
+        let districtItem = UIBarButtonItem(customView: districtView)
+        
+        // 监听点击事件
+        districtView.iconButton.addTarget(self, action: #selector(districtClick), forControlEvents: .TouchUpInside)
+        
+        return districtItem
+    }()
+    lazy var sortItem : UIBarButtonItem = {
+        // 初始化分类 Item
+        let sortView = CDH_BarButtonItemView.barButtonItemView(UIImage(named: "icon_sort")!, highlightedImage : UIImage(named: "icon_sort_highlighted" )!, title : "排序", subTitleLabel: "默认顺序")
+        
+        let sortItem = UIBarButtonItem(customView: sortView)
+        
+        // 监听点击事件
+        sortView.iconButton.addTarget(self, action: #selector(sortClick), forControlEvents: .TouchUpInside)
+        
+        return sortItem
+    }()
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +52,23 @@ class CDH_HomeViewController: UIViewController {
         
     }
 }
+
+// MARK: - 按钮点击事件
+extension CDH_HomeViewController{
+    @objc private func categoryClick(){
+        print("--categoryClick--")
+        
+    }
+    @objc private func districtClick(){
+        print("--districtClick--")
+    }
+    @objc private func sortClick(){
+        print("--sortClick--")
+    }
+}
+
+
+
 // MARK: - 布局子控件
 extension CDH_HomeViewController {
     // MARK: - 据导航栏
@@ -35,6 +79,6 @@ extension CDH_HomeViewController {
         logoItem.enabled = false
         
         // 2.添加到导航栏
-        navigationItem.leftBarButtonItems = [logoItem]
+        navigationItem.leftBarButtonItems = [logoItem, categoryItem, districtItem, sortItem]
     }
 }
--- MeiTuan/Classes/Views/CDH_BarButtonItemView.swift
+++ MeiTuan/Classes/Views/CDH_BarButtonItemView.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class CDH_BarButtonItemView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var iconButton: UIButton!
+    
+    // MARK: - 提供类方法快速创建
+    class func barButtonItemView( image : UIImage , highlightedImage : UIImage ,title : String , subTitleLabel : String) -> CDH_BarButtonItemView {
+        
+        // 1.通过xib 加载控件
+        let barButtonItemView = NSBundle.mainBundle().loadNibNamed("CDH_BarButtonItemView", owner: nil, options: nil).first as! CDH_BarButtonItemView
+        
+        // 2.初始化数据
+        barButtonItemView.titleLabel.text = title
+        barButtonItemView.subTitleLabel.text = subTitleLabel
+        barButtonItemView.iconButton.setImage(image, forState: .Normal)
+        barButtonItemView.iconButton.setImage(highlightedImage, forState: .Highlighted)
+        return barButtonItemView
     }
-    */
-
+    
+    override func awakeFromNib() {
+        ///  不要跟随父控件拉伸, 如果不设置关闭,则会跟随父控件拉伸, 甚至有可能被拉伸小时
+        autoresizingMask = UIViewAutoresizing.None
+        /// 也可以通过工具栏设置, 先取消 autoresizing , 在取消掉跟随父控件的拉伸宽高拉伸, 在选择 autoresizing, 这样既可不影响对子控件布局, 不可以达到不跟随父控件
+    }
 }
+
