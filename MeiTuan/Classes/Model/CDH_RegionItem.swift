98905a08a1995488f7c8d801aafc836e59b8123c
chendehao <958689927@qq.com>
2016-08-14 11:23:28 +0800

    区域模型 CDH_RegionItem

---
+++ MeiTuan/Classes/Model/CDH_RegionItem.swift
@@ -0,0 +1,15 @@
+//
+//  CDH_RegionItem.swift
+//  MeiTuan
+//
+//  Created by chendehao on 16/8/14.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_RegionItem: CDH_BaseItem {
+    
+    var name : String = ""
+    var subregions : [String]?
+}
