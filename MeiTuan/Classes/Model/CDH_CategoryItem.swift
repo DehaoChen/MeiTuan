1bf9d308635270834751c3b8c0ed8dbc8b29e0dd
chendehao <958689927@qq.com>
2016-08-14 11:19:07 +0800

    添加 CDH_CategoryItem 模型

---
+++ MeiTuan/Classes/Model/CDH_CategoryItem.swift
@@ -0,0 +1,21 @@
+
+//
+//  CDH_CategoryItem.swift
+//  MeiTuan
+//
+//  Created by chendehao on 16/8/14.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_CategoryItem: CDH_BaseItem {
+    var highlighted_icon : String = ""
+    var icon : String = ""
+    var name : String = ""
+    var small_highlighted_icon : String = ""
+    var small_icon : String = ""
+    
+    var map_icon : String = ""
+    var subcategories : [String]?
+}
