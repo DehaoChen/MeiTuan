bee089ee5cbfe57b482b298f8217549e4be81068
chendehao <958689927@qq.com>
2016-08-14 11:25:18 +0800

    排序模型 CDH_SortItem

---
+++ MeiTuan/Classes/Model/CDH_SortItem.swift
@@ -0,0 +1,14 @@
+//
+//  CDH_SortItem.swift
+//  MeiTuan
+//
+//  Created by chendehao on 16/8/14.
+//  Copyright © 2016年 CDHao. All rights reserved.
+//
+
+import UIKit
+
+class CDH_SortItem: CDH_BaseItem {
+    var label : String = ""
+    var value : Int = 0
+}
